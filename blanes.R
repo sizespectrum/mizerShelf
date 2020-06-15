ff472d13661629b51d5a721919dd72ebd1175e10
Gustav Delius <gustav.delius@gmail.com>
2020-06-15 08:00:29 +0100

    Adding controls for interaction with detritus and carrion to `tuneParams()`

--- blanes.R
+++ blanes.R
@@ -1,3 +1,18 @@
+remotes::install_github("sizespectrum/mizer")
+remotes::install_github("sizespectrum/mizerExperimental")
+
+library(mizerExperimental)
+
+setRho <- function(params) {
+    params@other_params$detritus$rho <-
+        outer(params@species_params$rho_detritus,
+              params@w^params@resource_params$n)
+    params@other_params$carrion$rho <-
+        outer(params@species_params$rho_carrion,
+              params@w^params@resource_params$n)
+    params
+}
+
 encounter_contribution <- function(params, n_other, component, ...) {
     params@other_params[[component]]$rho * n_other[[component]]
 }
@@ -5,3 +20,58 @@ encounter_contribution <- function(params, n_other, component, ...) {
 constant_dynamics <- function(params, n_other, component, ...) {
     n_other[[component]]
 }
+
+rhoControlUI <- function(p, sp) {
+    tagList(
+        tags$h3(tags$a(id = "rho"), "rho"),
+        sliderInput("rho_detritus", "rho_detritus", value = sp$rho_detritus,
+                    min = 0,
+                    max = signif(ifelse(sp$rho_detritus > 0,
+                                        sp$rho_detritus * 2,
+                                        0.001), 2)),
+        sliderInput("rho_carrion", "rho_carrion", value = sp$rho_carrion,
+                    min = 0,
+                    max = signif(ifelse(sp$rho_carrion > 0,
+                                        sp$rho_carrion * 2,
+                                        0.001), 2))
+    )
+}
+
+rhoControl <- function(input, output, session, params, flags) {
+    observeEvent(
+        list(input$rho_detritus, input$rho_carrion),
+        {
+            p <- params()
+            sp <- input$sp
+            if (!identical(sp, flags$sp_old_rho)) {
+                flags$sp_old_rho <- sp
+                return()
+            }
+            # Update slider min/max so that they are a fixed proportion of the
+            # parameter value
+            updateSliderInput(session, "rho_detritus",
+                              min = 0,
+                              max = signif(ifelse(input$rho_detritus > 0,
+                                                  input$rho_detritus * 2,
+                                                  0.001), 2))
+            updateSliderInput(session, "rho_carrion",
+                              min = 0,
+                              max = signif(ifelse(input$rho_carrion > 0,
+                                                  input$rho_carrion * 2,
+                                                  0.001), 2))
+
+            p@species_params[sp, "rho_detritus"]   <- input$rho_detritus
+            p@species_params[sp, "rho_carrion"]   <- input$rho_carrion
+            p <- setRho(p)
+            tuneParams_update_species(sp, p, params)
+        },
+        ignoreInit = TRUE)
+}
+
+controls <- list("egg", "predation", "rho", "fishing", "reproduction", "other",
+                 "interaction")
+
+params <- readRDS("params250520.rds")
+catch <- readRDS("catch.rds")
+
+tuneParams(params, catch = catch, controls = controls)
