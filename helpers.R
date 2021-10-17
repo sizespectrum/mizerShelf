fecd5203a095611eec6a997cf1bdb3ce723d8a36
Gustav Delius <gustav.delius@gmail.com>
2021-10-17 08:32:05 +0100

    Overwrite `scaleModel()` to also rescale rho.

--- helpers.R
+++ helpers.R
@@ -10,4 +10,14 @@ rescaleComponents <- function(params, carrion_factor = 1, detritus_factor = 1) {
         params@initial_n_other[["detritus"]] * detritus_factor
     params@other_params[["detritus"]]$rho <- 
         params@other_params[["detritus"]]$rho / detritus_factor
+    
+    params
+}
+
+scaleModel <- function(params, factor) {
+    params@other_params[["carrion"]]$rho <- 
+        params@other_params[["carrion"]]$rho / factor
+    params@other_params[["detritus"]]$rho <- 
+        params@other_params[["detritus"]]$rho / factor
+    mizer::scaleModel(params)
 }
