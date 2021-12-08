24442fa387dfe7bc291c241d8b8a99c00f77cb8d
Gustav Delius <gustav.delius@gmail.com>
2021-12-08 22:16:27 +0000

    helper to calculate relative difference

--- helpers.R
+++ helpers.R
@@ -26,9 +26,15 @@ scaleModel <- function(params, factor) {
 plot_diff <- function(x1, x2) {
     wc <- compare(x1, x2, max_diffs = Inf)
     if (length(wc)) {
-        barplot((x1 - x2) / x2, names.arg = 1:length(x1), horiz = TRUE)
+        barplot(rel_diff(x1, x2), names.arg = 1:length(x1), horiz = TRUE)
     }
     wc
+}
+
+rel_diff <- function(x1, x2) {
+    r <- abs((x1 - x2) / (x1 + x2))
+    r[is.nan(r)] <- 0
+    r
 }
 
 l <- function(w) {
