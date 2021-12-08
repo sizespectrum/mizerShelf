4a14c706b41877fd26014b0c540fc8b47119f379
Gustav Delius <gustav.delius@gmail.com>
2021-12-08 22:16:00 +0000

    Predation kernel that is truncated at 1 sigma. I played with it and it did not seem to make a huge difference.

--- rates.R
+++ rates.R
@@ -8,3 +8,13 @@ gearMort <- function(params, f_mort) {
     gear_mort[gear_mort < 0] <- 0
     gear_mort
 }
+
+# predation kernel that cuts off at 1 sigma
+cut_lognormal_pred_kernel <- function(ppmr, beta, sigma) {
+    Beta <- log(beta)
+    phi <- exp(-(log(ppmr) - Beta)^2 / (2 * sigma^2))
+    # rr is the maximal log predator/prey mass ratio
+    rr <- exp(Beta + sigma)
+    phi[ppmr > rr] <- 0
+    return(phi)
+}
