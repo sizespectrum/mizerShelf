2eaeac8089fa7fbbe98dac596a421b8885b8f840
Gustav Delius <gustav.delius@gmail.com>
2022-02-24 05:06:17 +0000

    Renaming species parameter `gearMort` to `gear_mort`

--- R/rates.R
+++ R/rates.R
@@ -4,7 +4,7 @@ seMort <- function(params, n, n_pp, n_other, t, f_mort, pred_mort, ...) {
 }
 
 gearMort <- function(params, f_mort) {
-    gear_mort <- params@species_params$gearMort - f_mort
+    gear_mort <- params@species_params$gear_mort - f_mort
     gear_mort[gear_mort < 0] <- 0
     gear_mort
 }
